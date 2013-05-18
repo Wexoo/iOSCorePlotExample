1037274883f90ae6685e86b2ef837cbf004f6d31
Philipp Weixlbaumer <WeixlbaumerP@WeixlbaumerPs-MacBook.local>
2013-05-19 00:08:06 +0200

    Initial Commit

---
+++ BitCoinNotifier/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  BitCoinNotifier
+//
+//  Created by Philipp Weixlbaumer on 5/19/13.
+//  Copyright (c) 2013 Philipp Weixlbaumer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ BitCoinNotifier/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  BitCoinNotifier
+//
+//  Created by Philipp Weixlbaumer on 5/19/13.
+//  Copyright (c) 2013 Philipp Weixlbaumer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
